2c94393be3b5895fc62219919cd4c39435349e41
Kathrine Kahns Hille <201708081@post.au.dk>
2019-03-12 13:57:45 +0100

    Oprettet Interfacet ICollisionStatus og oprettet klassen CollisionStatus som implementerer interfacet (uden fyldte metoder)

---
+++ SWT_Gruppe10_AirTraficMonitoring/CollisionStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT_Gruppe10_AirTraficMonitoring
+{
+    class CollisionStatus : ICollisionStatus
+    {
+        public void DetectCollision()
+        {
+
+        }
+
+        public void RaiseEvent()
+        {
+
+        }
+
+        public void RecieveData()
+        {
+
+        }
+
+        public void ShowData()
+        {
+
+        }
+    }
+}
