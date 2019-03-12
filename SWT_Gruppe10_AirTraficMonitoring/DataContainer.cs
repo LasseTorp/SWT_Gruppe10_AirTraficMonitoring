6872165fcea51a4ef8bb689b28af81c278ca9b4f
JeppeChr <jeppechristensen2@gmail.com>
2019-03-12 13:51:42 +0100

    SortTrackData - ISortTrackData - DataContainer

---
+++ SWT_Gruppe10_AirTraficMonitoring/DataContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SWT_Gruppe10_AirTraficMonitoring
+{
+    public class DataContainer : EventArgs
+    {
+
+        public string Tag { get; set; }
+        public int XCor { get; set; }
+        public int YCor { get; set; }
+        public int Altitude { get; set;  }
+        public DateTime TimeStamp { get; set; }
+        public int Velocity { get; set; }
+        public int Course { get; set; }
+        public string Collision { get; set; }
+        
+    }
+}
