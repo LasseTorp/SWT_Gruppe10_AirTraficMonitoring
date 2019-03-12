a2fa448fe8b5133d1c2b8a7af9e41ceb05a6dd56
MathiasAKAchampen <201509795@post.au.dk>
2019-03-12 13:53:24 +0100

    Add interface IDataCalculator, and class DataCalculator

---
+++ SWT_Gruppe10_AirTraficMonitoring/DataCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT_Gruppe10_AirTraficMonitoring
+{
+    class DataCalculator : IDataCalculator
+    {
+        public void calculate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateVelocity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void calculateCourse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
