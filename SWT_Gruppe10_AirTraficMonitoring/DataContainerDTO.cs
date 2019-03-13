12a9ad194fda1f9f7928240690e1f787a221a497
JeppeChr <jeppechristensen2@gmail.com>
2019-03-13 10:34:37 +0100

    AirTrafficEvent - DTO

--- SWT_Gruppe10_AirTraficMonitoring/DataContainerDTO.cs
+++ SWT_Gruppe10_AirTraficMonitoring/DataContainerDTO.cs
@@ -1,19 +1,22 @@
 ﻿using System;
-using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SWT_Gruppe10_AirTraficMonitoring
 {
-    public class DataContainer : EventArgs
+    public class DataContainerDTO
     {
 
         public string Tag { get; set; }
         public int XCor { get; set; }
         public int YCor { get; set; }
-        public int Altitude { get; set;  }
+        public int Altitude { get; set; }
         public DateTime TimeStamp { get; set; }
         public int Velocity { get; set; }
         public int Course { get; set; }
         public string Collision { get; set; }
-        
+
     }
 }
