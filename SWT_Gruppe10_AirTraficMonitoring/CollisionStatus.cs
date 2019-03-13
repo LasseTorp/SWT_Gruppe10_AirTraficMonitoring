8b5cddf3e60b593b3bf489a7533d3dd77776fde5
Kathrine Kahns Hille <201708081@post.au.dk>
2019-03-13 10:40:25 +0100

    Further work with the methods in the CollisionStatus Class

--- SWT_Gruppe10_AirTraficMonitoring/CollisionStatus.cs
+++ SWT_Gruppe10_AirTraficMonitoring/CollisionStatus.cs
@@ -8,17 +8,26 @@ namespace SWT_Gruppe10_AirTraficMonitoring
 {
     class CollisionStatus : ICollisionStatus
     {
+        private bool collisionStatus_;
+        private string aircraftsColliding_;
+
+        public CollisionStatus(IDataCalculator iDataCalculator)
+        {
+            iDataCalculator.DataCalculatedEvent += DetectCollision(); 
+        }
+
+        public void RecieveData()
+        {
+
+        }
+
         public void DetectCollision()
         {
+            
 
         }
 
         public void RaiseEvent()
-        {
-
-        }
-
-        public void RecieveData()
         {
 
         }
