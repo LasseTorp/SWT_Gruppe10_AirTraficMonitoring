73aac0897b98b14762c32f274dc8265af09db5db
Lasse Torp Jacobsen <Lasse.torp.jacobsen@post.au.dk>
2019-03-17 09:53:34 +0100

    Oprettet test class for TransponderReceiver

---
+++ AirTrafficMonitoring_Test_unit/TransponderReceiverTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+using SWT_Gruppe10_AirTraficMonitoring;
+
+
+namespace AirTrafficMonitoring_Test_unit
+{
+    public class TransponderReceiverTest
+    {
+        private ITransponderReceiver fakeTransponderReceiver_;
+
+        //HER MANGLER UUT
+
+        [SetUp]
+        public void Setup()
+        {
+            //Laver fake Transponder Data Receiver
+            fakeTransponderReceiver_ = Substitute.For<ITransponderReceiver>();
+            
+        }
+
+        [Test]
+        public void TestReception()
+        {
+
+        }
+    }
+}
