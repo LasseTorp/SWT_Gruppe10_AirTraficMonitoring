d968999c3a6c14140c87d3a83babf4359b1557a7
MathiasAKAchampen <201509795@post.au.dk>
2019-03-20 11:25:02 +0100

    Created testcases for calculateCourse and calculateVelocity

--- AirTrafficMonitoring_Test_unit/DataCalculatorTest.cs
+++ AirTrafficMonitoring_Test_unit/DataCalculatorTest.cs
@@ -22,28 +22,34 @@ namespace AirTrafficMonitoring_Test_unit
             UUT_ = new DataCalculator(fakeSortTrackData);
         }
 
-        [Test]
-        public void calculateCourse_start6000and6000End12000and12000_45degrees()
-        {
-            UUT_.oldTrackData.Add(new FlightDataDTO("ATR423", 6000, 6000, 7000, new DateTime(2019, 4, 17, 13, 30, 10),0, 0, "" ));
+        
 
-            FlightDataDTO flight = new FlightDataDTO("ATR423", 12000, 12000, 7000,
+        [TestCase(6000, 6000, 12000, 12000,45)]
+        [TestCase(12000, 12000, 6000, 6000, 315)]
+        public void calculateCourse_startandEnd_degrees(int s1, int s2, int s3, int s4, int expectedResult)
+        {
+            UUT_.oldTrackData.Add(new FlightDataDTO("ATR423", s1, s2, 7000, new DateTime(2019, 4, 17, 13, 30, 10),0, 0, "" ));
+
+            FlightDataDTO flight = new FlightDataDTO("ATR423", s3, s4, 7000,
                 new DateTime(2019, 4, 17, 14, 30, 10), 0, 0, "");
             UUT_.calculateCourse(flight);
 
-            Assert.That(flight.Course, Is.EqualTo(45));
+            Assert.That(flight.Course, Is.EqualTo(expectedResult));
         }
 
-        [Test]
-        public void calculateVelocity_XYdifference4000HighDifference3000Time20Sec_250meterprsecond()
+        [TestCase(6000, 6000, 7000, 6000, 10000, 4000, 250)]
+        [TestCase(6000, 6000, 4000, 6000, 10000, 7000, 250)]
+        [TestCase(6000, 10000, 7000, 6000, 6000, 4000, 250)]
+        [TestCase(6000, 10000, 4000, 6000, 6000, 7000, 250)]
+        public void calculateVelocity_XYdifferenceHighDifferenceTimeDifference_meterprsecond(int s1, int s2, int s3, int s4, int s5, int s6, int expectedResult)
         {
-            UUT_.oldTrackData.Add(new FlightDataDTO("BTU423", 6000, 6000, 7000, new DateTime(2019, 4, 17, 13, 30, 10), 0, 0, ""));
-            FlightDataDTO flight = new FlightDataDTO("BTU423", 6000, 10000, 4000,
+            UUT_.oldTrackData.Add(new FlightDataDTO("BTU423", s1, s2, s3, new DateTime(2019, 4, 17, 13, 30, 10), 0, 0, ""));
+            FlightDataDTO flight = new FlightDataDTO("BTU423", s4, s5, s6,
                 new DateTime(2019, 4, 17, 13, 30, 30), 0, 0, "");
 
             UUT_.CalculateVelocity(flight);
 
-            Assert.That(flight.Velocity, Is.EqualTo(250));
+            Assert.That(flight.Velocity, Is.EqualTo(expectedResult));
         }
 
     }
