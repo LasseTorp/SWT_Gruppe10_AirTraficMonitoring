de59363b2c23d8629090b601e1c033177d0b0598
Lasse Torp Jacobsen <Lasse.torp.jacobsen@post.au.dk>
2019-04-09 14:48:21 +0200

    Public interface IPrint added

--- SWT_Gruppe10_AirTraficMonitoring/IPrint.cs
+++ SWT_Gruppe10_AirTraficMonitoring/IPrint.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SWT_Gruppe10_AirTraficMonitoring
 {
-    interface IPrint
+    public interface IPrint
     {
         void ConsolePrint(object sender, AirTrafficEvent e);
     }
