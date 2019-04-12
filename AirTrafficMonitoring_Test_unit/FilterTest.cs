82824c69e5166857501fe0a23832d11bbd7bd5be
JeppeChr <jeppechristensen2@gmail.com>
2019-04-12 11:15:40 +0200

    FilterTest Done

--- AirTrafficMonitoring_Test_unit/FilterTest.cs
+++ AirTrafficMonitoring_Test_unit/FilterTest.cs
@@ -10,6 +10,9 @@ using SWT_Gruppe10_AirTraficMonitoring;
 namespace AirTrafficMonitoring_Test_unit
 {
 
+    // Ny testklasse til genaflevering 
+
+
     [TestFixture]
     class FilterTest
     {
@@ -44,15 +47,19 @@ namespace AirTrafficMonitoring_Test_unit
         }
 
         [Test]
-        public void FiltrateArea_AreaFlightLimit_within()
+        public void FiltrateArea_AreaFlightLimitWithin_eventRaised()
         {
             List<FlightDataDTO> flightlist = new List<FlightDataDTO>();
 
-            FlightDataDTO flight1 = new FlightDataDTO("ABC123", 10000, 10000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
-            FlightDataDTO flight2 = new FlightDataDTO("DEF456", 70000, 15000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight1 = new FlightDataDTO("ABC123", 85000 , 5000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight2 = new FlightDataDTO("DEF456", 85000, 15000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight3 = new FlightDataDTO("GHI789", 80000, 5000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            
 
             flightlist.Add(flight1);
             flightlist.Add(flight2);
+            flightlist.Add(flight3);
+            
 
             //flightlist = UUT_.SortedDataTracks;
             sortTrackData_.SortDataEvent += Raise.EventWith(new AirTrafficEvent(flightlist));
@@ -63,7 +70,34 @@ namespace AirTrafficMonitoring_Test_unit
         }
 
         [Test]
-        public void AreaFligtLimit_outside()
+        public void FiltrateArea_AreaFlightLimitWithinAndOutside_eventRaised()
+        {
+            List<FlightDataDTO> flightlist = new List<FlightDataDTO>();
+
+            FlightDataDTO flight1 = new FlightDataDTO("ABC123", 85000, 5000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight2 = new FlightDataDTO("DEF456", 85000, 15000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight3 = new FlightDataDTO("GHI789", 80000, 5000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight4 = new FlightDataDTO("AAA456", 90000, 4000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight5 = new FlightDataDTO("BBB123", 85000, 4000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight6 = new FlightDataDTO("CCC555", 90000, 5000, 10000, DateTime.Now, 200, 90, new CollidingFlightsDTO("", "", ""));
+
+            flightlist.Add(flight1);
+            flightlist.Add(flight2);
+            flightlist.Add(flight3);
+            flightlist.Add(flight4);
+            flightlist.Add(flight5);
+            flightlist.Add(flight6);
+
+            //flightlist = UUT_.SortedDataTracks;
+            sortTrackData_.SortDataEvent += Raise.EventWith(new AirTrafficEvent(flightlist));
+
+            //Assert.That(UUT_.FiltratedDataTracks,Is.EqualTo(flightlist));
+            Assert.That(event_, Is.Not.Null);
+
+        }
+
+        [Test]
+        public void FiltrateArea_AreaFlightLimitOutside_noEventRaised()
         {
             List<FlightDataDTO> flightlist = new List<FlightDataDTO>();
 
@@ -82,12 +116,6 @@ namespace AirTrafficMonitoring_Test_unit
             //Assert.That(UUT_.FiltratedDataTracks,Is.Null);
             Assert.That(event_, Is.Null);
 
-        }
-
-        [Test]
-        public void TestestesVois()
-        {
-            Assert.That(event_, Is.Null);
         }
 
     }
