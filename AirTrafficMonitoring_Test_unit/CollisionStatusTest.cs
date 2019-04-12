6e10a98fd5478e7b3904a153e449025c3c389975
Kathrine Kahns Hille <201708081@post.au.dk>
2019-04-12 10:42:56 +0200

    Retested in the CollisionStatusTest class. Wrote tests for the different parts of CollisionsStatus class

--- AirTrafficMonitoring_Test_unit/CollisionStatusTest.cs
+++ AirTrafficMonitoring_Test_unit/CollisionStatusTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NSubstitute;
+using NSubstitute.Exceptions;
 using NUnit.Framework;
 using SWT_Gruppe10_AirTraficMonitoring;
 
@@ -13,14 +14,17 @@ namespace AirTrafficMonitoring_Test_unit
     class CollisionStatusTest
     {
         private CollisionStatus UUT_;
-        private IDataCalculator fakeDataCalculator;
+        private AirTrafficEvent airTrafficEvent_;
+        private IDataCalculator dataCalculator_; 
 
         [SetUp]
         public void SetUp()
         {
-            fakeDataCalculator = Substitute.For<IDataCalculator>(); 
-            UUT_ = new CollisionStatus(fakeDataCalculator);
-            UUT_.collisionStatus_ = false; 
+            airTrafficEvent_ = null;
+            dataCalculator_ = NSubstitute.Substitute.For<IDataCalculator>();
+            UUT_ = new CollisionStatus(dataCalculator_);
+
+            UUT_.CollisionStatusEvent += (e, args) => { airTrafficEvent_ = args; };
         }
 
         [TestCase(10000, 11000, 7000, 18000, 11000, 7000, false)]
@@ -75,9 +79,86 @@ namespace AirTrafficMonitoring_Test_unit
 
             Data_.Add(new FlightDataDTO("ABCD", 10, 10, 10, DateTime.Now, 10, 10, new CollidingFlightsDTO("", "", "")));
 
-            fakeDataCalculator.DataCalculatedEvent += Raise.EventWith(this, new AirTrafficEvent(Data_));
+            dataCalculator_.DataCalculatedEvent += Raise.EventWith(this, new AirTrafficEvent(Data_));
 
             Assert.That(UUT_.aircraftList, Is.EqualTo(Data_));
         }
+
+
+        //The following is the work that has been done before handing in the first time and handing it in again 
+        [Test]
+        public void testRecieveFunction()
+        {
+            List<FlightDataDTO> listOfFlightData = new List<FlightDataDTO>();
+
+            FlightDataDTO flightData1 = new FlightDataDTO("ABC123", 23000, 27000, 1500, DateTime.Now, 50, 0, new CollidingFlightsDTO("","",""));
+            FlightDataDTO flightData2 = new FlightDataDTO("DEF456", 35000, 38000, 1800, DateTime.Now, 50, 0, new CollidingFlightsDTO("","",""));
+
+            listOfFlightData.Add(flightData1);
+            listOfFlightData.Add(flightData2);
+
+            dataCalculator_.DataCalculatedEvent += Raise.EventWith(new AirTrafficEvent(listOfFlightData));
+
+            Assert.That(airTrafficEvent_, Is.Not.Null);
+        }
+
+        [TestCase("ABC123", "DEF456", 10000, 11000, 20000, 10000, 11000, 7000, "", "")]
+        [TestCase("ABC123", "DEF456", 10000, 11000, 7000, 10000, 11000, 7200, "ABC123", "DEF456")]
+        [TestCase("ABC123", "DEF456", 10000, 11000, 7000, 10000, 11000, 7300, "ABC123", "DEF456")]
+        [TestCase("ABC123", "DEF456", 10000, 11000, 7000, 10000, 11000, 7301, "", "")]
+        public void detectCollisionDependingOnAltitude(string flightTag1Test, string flightTag2Test, int x1, int y1, int altitude1, int x2, int y2, int altitude2, string expectedTag1, string expectedTag2)
+        {
+            List<FlightDataDTO> listOfFlightData = new List<FlightDataDTO>();
+
+            FlightDataDTO flight1 = new FlightDataDTO(flightTag1Test, x1, y1, altitude1, new DateTime(2019, 4, 17, 14, 30, 40), 0, 0, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight2 = new FlightDataDTO(flightTag2Test, x2, y2, altitude2, new DateTime(2019, 4, 17, 14, 30, 40), 0, 0, new CollidingFlightsDTO("", "", ""));
+            listOfFlightData.Add(flight1);
+            listOfFlightData.Add(flight2);
+
+            dataCalculator_.DataCalculatedEvent += Raise.EventWith(new AirTrafficEvent(listOfFlightData)); 
+
+            //Assert.That(airTrafficEvent_.AirTrafficList[0].CollidingFlightsDto.flightTag1, Is.EqualTo(expectedTag1));
+            //Assert.That(airTrafficEvent_.AirTrafficList[0].CollidingFlightsDto.flightTag2, Is.EqualTo(expectedTag2));
+
+            Assert.IsTrue((airTrafficEvent_.AirTrafficList[0].CollidingFlightsDto.flightTag1 == expectedTag1) && (airTrafficEvent_.AirTrafficList[0].CollidingFlightsDto.flightTag2 == expectedTag2));
+        }
+
+        [TestCase("ABC123", "DEF456", 10000, 11000, 9000, 15001, 11000, 9000, "", "")] //Boundary
+        [TestCase("ABC123", "DEF456", 10000, 11000, 9000, 15000, 11000, 9000, "ABC123", "DEF456")] //Boundary
+        [TestCase("ABC123", "DEF456", 10000, 11000, 9000, 11000, 11000, 9000, "ABC123", "DEF456")] //Far too close
+        [TestCase("ABC123", "DEF456", 10000, 11000, 9000, 27000, 11000, 9000, "", "")] //Far too far away 
+        public void detectCollisionDependingOnXCoordinates(string flightTag1Test, string flightTag2Test, int x1, int y1, int altitude1, int x2, int y2, int altitude2, string expectedTag1, string expectedTag2)
+        {
+            List<FlightDataDTO> listOfFlightData = new List<FlightDataDTO>();
+
+            FlightDataDTO flight1 = new FlightDataDTO(flightTag1Test, x1, y1, altitude1, new DateTime(2019, 4, 17, 14, 30, 40), 0, 0, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight2 = new FlightDataDTO(flightTag2Test, x2, y2, altitude2, new DateTime(2019, 4, 17, 14, 30, 40), 0, 0, new CollidingFlightsDTO("", "", ""));
+            listOfFlightData.Add(flight1);
+            listOfFlightData.Add(flight2);
+
+            dataCalculator_.DataCalculatedEvent += Raise.EventWith(new AirTrafficEvent(listOfFlightData));
+
+            Assert.IsTrue((airTrafficEvent_.AirTrafficList[0].CollidingFlightsDto.flightTag1 == expectedTag1) && (airTrafficEvent_.AirTrafficList[0].CollidingFlightsDto.flightTag2 == expectedTag2));
+        }
+
+        [TestCase("ABC123", "DEF456", 10000, 11000, 9000, 10000, 16001, 9000, "", "")] //Boundary
+        [TestCase("ABC123", "DEF456", 10000, 11000, 9000, 15000, 16000, 9000, "ABC123", "DEF456")] //Boundary
+        [TestCase("ABC123", "DEF456", 10000, 11000, 9000, 10000, 12000, 9000, "ABC123", "DEF456")] //Far too close
+        [TestCase("ABC123", "DEF456", 10000, 11000, 9000, 27000, 20000, 9000, "", "")] //Far too far away 
+        public void detectCollisionDependingOnYCoordinates(string flightTag1Test, string flightTag2Test, int x1, int y1, int altitude1, int x2, int y2, int altitude2, string expectedTag1, string expectedTag2)
+        {
+            List<FlightDataDTO> listOfFlightData = new List<FlightDataDTO>();
+
+            FlightDataDTO flight1 = new FlightDataDTO(flightTag1Test, x1, y1, altitude1, new DateTime(2019, 4, 17, 14, 30, 40), 0, 0, new CollidingFlightsDTO("", "", ""));
+            FlightDataDTO flight2 = new FlightDataDTO(flightTag2Test, x2, y2, altitude2, new DateTime(2019, 4, 17, 14, 30, 40), 0, 0, new CollidingFlightsDTO("", "", ""));
+            listOfFlightData.Add(flight1);
+            listOfFlightData.Add(flight2);
+
+            dataCalculator_.DataCalculatedEvent += Raise.EventWith(new AirTrafficEvent(listOfFlightData));
+
+            Assert.IsTrue((airTrafficEvent_.AirTrafficList[0].CollidingFlightsDto.flightTag1 == expectedTag1) && (airTrafficEvent_.AirTrafficList[0].CollidingFlightsDto.flightTag2 == expectedTag2));
+        }
     }
+
+
 }
